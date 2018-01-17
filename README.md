a4e5e7e6adaf9265e311149e88ccd0f1f0667155
Alison Bushor <AliB@Alisons-MacBook-Pro.local>
2018-01-16 19:23:52 -0500

    Work on Homepage and Projects

---
+++ README.md
@@ -0,0 +1,9 @@
+# README
+
+Preliminary portfolio design made with Ruby on Rails. 
+
+Linear CMYK design with a strong Helvetica typeface. 
+
+Home // Info // Project // Project(id) 
+
+Includes PNGs to be used with as design elements. 
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+
+  resources :info, only: [:index]
+  resources :home, only: [:index]
+  resources :projects, only: [:index, :show]
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
