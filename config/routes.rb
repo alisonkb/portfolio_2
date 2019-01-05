df6a28855d601ec1a0183f1afc085cbf1498063a
Alison Bushor <alisonbushor@gmail.com>
2019-01-05 16:23:53 -0500

    add work controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
 
+  # get 'work/index'
+
   resources :info, only: [:index]
   resources :home, only: [:index], :path => "/"
   resources :projects, only: [:index, :show]
   resources :media, only: [:index]
+  resources :work, only: [:index]
 
 
 
